f3e552b1f4b03072b18c6cf6d5ef8835f971aa32
Анастасия Мамаева <hdekjsne@gmail.com>
2023-10-27 12:00:51 +0300

    ArraySchema length

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,11 +1,17 @@
 export default class ArraySchema {
 	constructor() {
-		this.length = false;
+		this.l = null;
+	}
+
+	length(l) {
+		this.l = l;
+		return this;
 	}
 
 	isValid(value) {
 		const validations = [
 			Array.isArray(value),
+			this.l !== null ? value?.length === this.l : true,
 		];
 		return !validations.includes(false);
 	}
