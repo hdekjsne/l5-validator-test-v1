2b0cf281db0cf4eabe3343fb81a8ce06bf7e6f53
Анастасия Мамаева <hdekjsne@gmail.com>
2023-10-27 11:40:48 +0300

    odd & even methods for NumberSchema

--- src/NumberSchema.js
+++ src/NumberSchema.js
@@ -1,7 +1,29 @@
 export class NumberSchema {
-	constructor() {}
+	constructor() {
+		this.opts = {
+			even: false, // чётное
+			odd: false, // нечётное
+		}
+	}
+
+	even() {
+		this.opts.even = true;
+		this.opts.odd = false;
+		return this;
+	}
+
+	odd() {
+		this.opts.odd = true;
+		this.opts.even = false;
+		return this;
+	}
 
 	isValid(value) {
-		return typeof value === 'number';
+		const validations = [
+			typeof value === 'number',
+			this.opts.even ? value % 2 === 0 : true,
+			this.opts.odd ? value % 2 !== 0 : true,
+		]
+		return !validations.includes(false);
 	}
 }
