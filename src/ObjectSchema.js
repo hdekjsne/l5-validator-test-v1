e814355a96f1e96b470344a5e9124f2aaddcadd6
Анастасия Мамаева <hdekjsne@gmail.com>
2023-10-27 12:12:39 +0300

    finish the last task

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,7 +1,22 @@
+function checkKeys() {}
+
 export default class ObjectSchema {
-	constructor() {}
+	constructor() {
+		this.example = null;
+	}
 
-	shape() {}
+	shape(obj) {
+		this.example = obj;
+		return this;
+	}
 
-	isValid() {}
+	isValid(obj) {
+		const checkedKeys = Object.entries(this.example)
+			.map(([key, condition]) => condition.isValid(obj[key]));
+		const validations = [
+			this.example !== null ? Object.keys(obj).length === Object.keys(this.example).length : true,
+			...checkedKeys,
+		];
+		return !validations.includes(false);
+	}
 }
